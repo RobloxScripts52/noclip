449ca174519b07497f6d9ee433e5683194518e43
RobloxScripts52 <83404059+RobloxScripts52@users.noreply.github.com>
2021-04-29 13:24:38 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,18 +7,18 @@ end)
 plr = game.Players.LocalPlayer
 mouse = plr:GetMouse()
 mouse.KeyDown:connect(function(key)
-if key == "e" then
+if key == "q" then
 noclip = not noclip
 game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
 end
 end)
 game.StarterGui:SetCore("SendNotification", {
-	Title = "Noclip";
-	Text = "Loaded.";
-	Duration = "10";
+Title = "Noclip";
+Text = "Loaded.";
+Duration = "10";
 })
 game.StarterGui:SetCore("SendNotification", {
-	Title = "Noclip";
-	Text = "Press E To Noclip";
-	Duration = "10";
+Title = "Noclip";
+Text = "Press Q To Noclip";
+Duration = "10";
 })
